b7dba684764639fe83397db56b329307d71ff0b8
nagatani-aoi <224x218x@stu.kobe-u.ac.jp>
2022-09-14 12:09:29 +0900

    修正

--- src/app.py
+++ src/app.py
@@ -10,7 +10,19 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 import os
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
+
+# app = Flask(__name__)
+# app.config["SECRET_KEY"] = b'_5#y2L"F4Q8z\n\xec]dasfe/'
+# app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///flask_blog.db'
+# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+
+# db = SQLAlchemy(app)
+
+# class Entry(db.Model):
+#     __tablename__ = 'words'
+#     id = db.Column(db.Integer, primary_key=True)
+#     word = db.Column(db.String(50), unique=True)
 
 app = Flask(__name__)
 
@@ -42,9 +54,15 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+    if "test" in event.message.text :
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="作動中"))
+
+    else :
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=event.message.text))
 
 #プッシュメッセージ
 @app.route("/send/<message>")
